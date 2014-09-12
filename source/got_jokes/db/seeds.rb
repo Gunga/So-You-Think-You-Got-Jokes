906d427c0653d62dee884ea70fc1998f4480b112
& <pair+dbradleyfl.sarahlshowers@devbootcamp.com>
2014-09-12 10:42:08 -0700

    test

--- source/got_jokes/db/seeds.rb
+++ source/got_jokes/db/seeds.rb
@@ -1,4 +1,8 @@
-Joke.create(description: "")
+Joke.create(description: "Why are there fences around a graveyard?")
+
+Joke.create(description: "Why do ghosts love elevators?")
+
+Joke.create(description: "Where do Volkswagens go when they get old?")
 
 Joke.create(description: "")
 
@@ -20,15 +24,11 @@ Joke.create(description: "")
 
 Joke.create(description: "")
 
-Joke.create(description: "")
+Punchline.create(description: "Because people are dying to get in.", joke_id:  , score:  )
 
-Joke.create(description: "")
+Punchline.create(description: "Because they lift their sprits.", joke_id:  , score:  )
 
-Punchline.create(description: "", joke_id:  , score:  )
-
-Punchline.create(description: "", joke_id:  , score:  )
-
-Punchline.create(description: "", joke_id:  , score:  )
+Punchline.create(description: "The old Volks home.", joke_id:  , score:  )
 
 Punchline.create(description: "", joke_id:  , score:  )
 
