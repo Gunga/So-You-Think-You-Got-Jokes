b0235ac75c98e57b18386f8ef960c7dbfa4bf75c
Devon Bradley <dbradleyfl@gmail.com>
2014-09-12 10:56:32 -0700

    added mine

--- source/got_jokes/db/seeds.rb
+++ source/got_jokes/db/seeds.rb
@@ -9,21 +9,21 @@ Joke.create(description: "")
 Joke.create(description: "")
 
 Joke.create(description: "")
-
+# sarah abover
 Joke.create(description: "")
 
 Joke.create(description: "")
 
 Joke.create(description: "")
 
-Joke.create(description: "")
+Joke.create(description: "Why did the turkey cross the road?")
 
-Joke.create(description: "")
+Joke.create(description: "Why did the calf cross the road?")
 
-Joke.create(description: "")
+Joke.create(description: "Why did the blind blonde cross the road?")
 
-Joke.create(description: "")
-
+Joke.create(description: "Why did the one-handed man cross the road?")
+#devon above
 Punchline.create(description: "Because people are dying to get in.", joke_id:  , score:  )
 
 Punchline.create(description: "Because they lift their sprits.", joke_id:  , score:  )
@@ -58,19 +58,19 @@ Punchline.create(description: "", joke_id:  , score:  )
 
 Punchline.create(description: "", joke_id:  , score:  )
 
-Punchline.create(description: "", joke_id:  , score:  )
+Punchline.create(description: "Because he was going to thanksgiving dinner", joke_id: 9, score: -2)
 
-Punchline.create(description: "", joke_id:  , score:  )
+Punchline.create(description: "Because he wasn't a chicken.", joke_id: 9, score: 2)
 
-Punchline.create(description: "", joke_id:  , score:  )
+Punchline.create(description: "To go to chick-fila", joke_id: 10, score: -2)
 
-Punchline.create(description: "", joke_id:  , score:  )
+Punchline.create(description: "To get to the udder side.", joke_id: 10, score: 2)
 
-Punchline.create(description: "", joke_id:  , score:  )
+Punchline.create(description: "She was goin to forever 21.", joke_id: 11, score: -2)
 
-Punchline.create(description: "", joke_id:  , score:  )
+Punchline.create(description: "She was following her seeing-eye chicken.", joke_id: 11, score: 2)
 
-Punchline.create(description: "", joke_id:  , score:  )
+Punchline.create(description: "To get to the hospital.", joke_id: 12, score: -2)
 
-Punchline.create(description: "", joke_id:  , score:  )
+Punchline.create(description: "To get to the second hand shop.", joke_id: 12, score: 2)
 
